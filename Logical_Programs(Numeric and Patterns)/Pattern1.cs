3e164f53e39eb664ae0cb054f239abb088f709ef
Vignesh12-12 <jackyvicky66@gmail.com>
2025-01-11 15:14:13 +0530

    Added new program

---
+++ Logical_Programs(Numeric and Patterns)/Pattern1.cs
@@ -0,0 +1,22 @@
+// 1. Write a program to print the following Pattern
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+// Program:
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = 4,k=1;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(k + " ");
+                k++;
+            }
+            Console.WriteLine();
+        }
+    }
+}
