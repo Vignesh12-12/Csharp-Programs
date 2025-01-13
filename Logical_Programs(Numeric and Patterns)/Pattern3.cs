05d250515a8e41d0a75febed514e47c87e0fb77c
Vignesh12-12 <jackyvicky66@gmail.com>
2025-01-13 12:01:15 +0530

    Added new program

---
+++ Logical_Programs(Numeric and Patterns)/Pattern3.cs
@@ -0,0 +1,25 @@
+// Write a program to print the following Pattern
+// * * * * *
+// *       *
+// *	   *
+// *	   *
+// * * * * *
+// Program:
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = 5;
+        for (int i = 1; i <=n; i++)
+        {
+            for (int j = 1; j <=n; j++)
+            {
+                if (i == 1 || j == 1 || i == n || j == n)
+                    Console.Write("*");
+                else
+                    Console.Write(" ");
+            }
+            Console.WriteLine();
+        } 
+    }
+}
